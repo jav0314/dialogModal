7065da8a2aa2615c8cd64d11af0f337e37270833
JavierM <144504968+jav0314@users.noreply.github.com>
2024-06-29 18:51:02 -0500

    getList ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './pages/principal/principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PrincipalComponent },
+  { path: 'principal', component: PrincipalComponent },
+];
--- src/app/pages/principal/principal.component.ts
+++ src/app/pages/principal/principal.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { PersonService } from '../../service/person.service';
+import { Person } from '../../models/person';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+  ],
   templateUrl: './principal.component.html',
-  styleUrl: './principal.component.css'
+  styleUrl: './principal.component.css',
 })
 export class PrincipalComponent {
+  private personService = inject(PersonService);
+  public listaPerson: Person[] = [];
+  public displayedColumns: string[] = ['id', 'name', 'email', 'action'];
+  ngOnInit() {
+    this.getList();
+  }
 
+  getList() {
+    this.personService.detailsGet().subscribe({
+      next: (data) => {
+        if (data.length > 0) {
+          this.listaPerson = data;
+        }
+      },
+      error: (err) => {
+        console.log(err + 'error');
+      },
+    });
+  }
+  editPerson(id: any) {}
+  deletePerson(id: any) {}
 }
